6d8c3c772b709af05ef5306442271aabf6a2edec
Rick Winfrey <rewinfrey@github.com>
2020-04-02 17:50:19 -0700

    Add few more cases and update names to be KingJames for patrickt

--- cmd/main.go
+++ cmd/main.go
@@ -23,7 +23,7 @@ func main() {
 	if err3 != nil {
 		panic(err3)
 	}
-	if err := tx.QueryRowContext(context.Background(), "INSERT INTO users (name, namey, age) OUTPUT inserted.id VALUES (@p1, @p1, @p2)", "Rick", 37).Scan(&id); err != nil {
+	if err := tx.QueryRowContext(context.Background(), "INSERT INTO users (name, namey, age) OUTPUT inserted.id VALUES (@p1, @p1, @p2)", "KingJames", 37).Scan(&id); err != nil {
 		panic(err)
 	}
 	fmt.Println(id)
@@ -31,8 +31,8 @@ func main() {
 
 	// limit := 1
 
-	// rows, err := db.DB().QueryContext(context.Background(), "SELECT id FROM users WHERE name = @p1 ORDER BY id ASC OFFSET 0 ROWS FETCH NEXT "+strconv.Itoa(limit)+" ROWS ONLY", "Rick")
-	rows, err := db.DB().QueryContext(context.Background(), "SELECT id FROM users WHERE name = @p1 AND namey = @p1 ORDER BY id ASC OFFSET 0 ROWS FETCH FIRST @p2 ROWS ONLY", "Rick", 1)
+	// rows, err := db.DB().QueryContext(context.Background(), "SELECT id FROM users WHERE name = @p1 ORDER BY id ASC OFFSET 0 ROWS FETCH NEXT "+strconv.Itoa(limit)+" ROWS ONLY", "KingJames")
+	rows, err := db.DB().QueryContext(context.Background(), "SELECT id FROM users WHERE name = @p1 AND namey = @p1 ORDER BY id ASC OFFSET 0 ROWS FETCH FIRST @p2 ROWS ONLY", "KingJames", 1)
 	if err != nil {
 		panic(err)
 	}
@@ -76,23 +76,34 @@ func main() {
 
 	var users2 []models.User
 	fmt.Println("where with find:")
-	db.Where("name = @p1", "Rick").Find(&users2)
+	db.Where("name = @p1", "KingJames").Find(&users2)
 	fmt.Println(users2)
 
 	var users3 []models.User
 	fmt.Println("limit with where and find:")
-	db.Limit(2).Where("name = @p1", "Rick").Find(&users3) // Error: mssql: Invalid usage of the option NEXT in the FETCH statement.
+	db.Limit(2).Where("name = @p1", "KingJames").Find(&users3) // Error: mssql: Invalid usage of the option NEXT in the FETCH statement.
 	fmt.Println(users3)
 
 	var users4 []models.User
-	fmt.Println("scope with where")
-	db.Scopes(nameForScope).Where("namey = @p1", "Rick").Find(&users4)
+	fmt.Println("scope with where:")
+	db.Scopes(nameForScope).Where("namey = @p1", "KingJames").Find(&users4)
 	fmt.Println(users4)
 
 	var users5 []models.User
-	fmt.Println("scope with where and limit")
-	db.Limit(2).Scopes(nameForScope).Where("namey = @p1", "Rick").Find(&users5) // Error: mssql: Invalid usage of the option NEXT in the FETCH statement.
+	fmt.Println("scope with where and limit:")
+	db.Limit(2).Scopes(nameForScope).Where("namey = @p1", "KingJames").Find(&users5) // Error: mssql: Invalid usage of the option NEXT in the FETCH statement.
 	fmt.Println(users5)
+
+	var users6 []models.User
+	fmt.Println("scope with where:")
+	db.Scopes(nameForScope, fakeLimit).Where("namey = @p1", "KingJames").Find(&users6) // Error: mssql: Invalid usage of the option NEXT in the FETCH statement.
+	fmt.Println(users6)
+
+	// Because we cannot user a Scope for the order by / fetch dance, we then have to resort to `Raw` as the Gorm query interface:
+	var users7 []models.User
+	fmt.Println("raw with order / fetch:")
+	db.Raw("SELECT * FROM users WHERE name = @p1 ORDER BY id DESC OFFSET 0 ROWS FETCH NEXT 4 ROWS ONLY", "KingJames").Find(&users7)
+	fmt.Println(users7)
 
 	// Another quirk about this is errors don't seem to propagate unless a follow up query is issued, so I'm using this placeholder as the last query issued to force any errors to be reported.
 	var endUser models.User
@@ -104,5 +115,9 @@ func main() {
 }
 
 func nameForScope(db *gorm.DB) *gorm.DB {
-	return db.Where("name = @p1", "Rick")
+	return db.Where("name = @p1", "KingJames")
+}
+
+func fakeLimit(db *gorm.DB) *gorm.DB {
+	return db.Where("ORDER BY id DESC OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY")
 }
