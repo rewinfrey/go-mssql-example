97acbd165f891fd0610ed5d870d07a7fca277a2e
Rick Winfrey <rewinfrey@github.com>
2020-04-02 23:59:38 -0700

    Uncomment out sql instructions

--- db/example.sql
+++ db/example.sql
@@ -1,8 +1,6 @@
--- CREATE DATABASE example
-
--- USE example
+CREATE DATABASE example
+USE example
 DROP TABLE IF EXISTS users
-
 CREATE TABLE
   users (
     id BIGINT IDENTITY NOT NULL PRIMARY KEY,
